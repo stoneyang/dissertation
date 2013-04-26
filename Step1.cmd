ee1d012d2e7b4bcbee54c1e75a046deea399b4b1
Fan Yang <stoneyang0915@gmail.com>
2013-04-26 12:58:33 +0800

    EDIT: Revised Chap 1 and References, and improved the Step1.cmd

--- Step1.cmd
+++ Step1.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-call del /Q /S /F *.aux *.bbl *.blg *.brf *.lof *.log *.lot *.out *.pdf *.thm *.toc
+call del /Q /S /F *.aux *.bak *.bbl *.blg *.brf *.lof *.log *.lot *.out *.pdf *.thm *.toc
 call pdflatex -c-style-errors mythesis_UTF8.tex
 call bibtex mythesis_UTF8.aux
