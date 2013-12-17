51d73ec876e2c7779551d70b831979077120a3b8
Fan Yang <stoneyang0915@gmail.com>
2013-12-17 11:30:46 +0800

    ENH: partially edited build process

--- Step1.cmd
+++ Step1.cmd
@@ -3,3 +3,8 @@
 call del /Q /S /F *.aux *.bak *.bbl *.blg *.brf *.lof *.log *.lot *.out *.pdf *.thm *.toc
 call pdflatex -c-style-errors mythesis_UTF8.tex
 call bibtex mythesis_UTF8.aux
+call pdflatex -c-style-errors mythesis_UTF8.tex
+call pdflatex -c-style-errors mythesis_UTF8.tex
+call "C:\Program Files\Adobe\Reader 11.0\Reader\AcroRd32.exe" mythesis_UTF8.pdf
+call "C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe" mythesis_UTF8.pdf
+call del /Q /S /F *.aux *.bak *.bbl *.blg *.brf *.lof *.log *.lot *.out *.thm *.toc
