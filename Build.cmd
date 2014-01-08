9ef52904134eb6988b31f29ec591123201209d47
Fan Yang <stoneyang0915@gmail.com>
2014-01-08 20:08:29 +0800

    ENH: initial commit of the new template

--- Build.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-call del /Q /S *.aux *.bbl *.blg *.brf *.gz *.lof *.log *.lot* *.out mythesis_UTF8.pdf *.thm *.toc
-call pdflatex -c-style-errors mythesis_UTF8
-call bibtex mythesis_UTF8
-call pdflatex -c-style-errors mythesis_UTF8
-call pdflatex -c-style-errors mythesis_UTF8
-call del /Q /S *.aux *.bbl *.blg *.brf *.gz *.lof *.log *.lot* *.out *.thm *.toc
---
+++ Clean_after_build.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+call del /Q /S *.aux *.bak *.bbl *.blg *.gz *.gz(busy) *.lof *.log *.lot *.toc *.out
+REM call latex -synctex=1 mythesis.tex
+REM call bibtex mythesis.aux
+REM call latex -synctex=1 mythesis.tex
+REM call latex -synctex=1 mythesis.tex
+REM call dvipdfmx mythesis.dvi
+REM call del /Q /S *.aux *.bak *.bbl *.blg *.gz *.lof *.log *.lot *.toc *.out
