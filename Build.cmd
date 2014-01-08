9b312847faf5ac1030ff367361fd4912564f8329
Fan Yang <stoneyang0915@gmail.com>
2014-01-08 16:02:40 +0800

    ENH: added abstracts

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-call del /Q /S *.aux *.bbl *.blg *.brf *.gz *.lof *.log *.lot* *.out *.pdf *.thm *.toc
+call del /Q /S *.aux *.bbl *.blg *.brf *.gz *.lof *.log *.lot* *.out mythesis_UTF8.pdf *.thm *.toc
 call pdflatex -c-style-errors mythesis_UTF8
 call bibtex mythesis_UTF8
 call pdflatex -c-style-errors mythesis_UTF8
