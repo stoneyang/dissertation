20820576c6e1e11c5946237495cf92aa5f0bee86
Fan Yang <stoneyang0915@gmail.com>
2014-10-14 23:23:15 +0800

    FIX: chinese character issue

--- presentation/slides/Build_slides.cmd
+++ presentation/slides/Build_slides.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
 
-del /F /Q defense_slides defense_slides.aux defense_slides.dvi defense_slides.log defense_slides.nav defense_slides.out defense_slides.out.bak defense_slides.pdf defense_slides.snm defense_slides.toc defense_slides
-latex defense_slides
-latex defense_slides
-dvips defense_slides
-ps2pdf -dPDFSETTINGS#/prepress -dCompatibilityLevel#1.4 -dSubsetFonts#true -dEmbedAllFonts#true defense_slides
-del /F /Q defense_slides defense_slides.aux defense_slides.log defense_slides.nav defense_slides.out defense_slides.out.bak defense_slides.snm defense_slides.toc
+del /F /Q slides_utf8 slides_utf8.aux slides_utf8.dvi slides_utf8.log slides_utf8.nav slides_utf8.out slides_utf8.out.bak slides_utf8.pdf slides_utf8.snm slides_utf8.toc
+latex slides_utf8
+latex slides_utf8
+dvips slides_utf8
+ps2pdf -dPDFSETTINGS#/prepress -dCompatibilityLevel#1.4 -dSubsetFonts#true -dEmbedAllFonts#true slides_utf8
+del /F /Q slides_utf8 slides_utf8.aux slides_utf8.log slides_utf8.nav slides_utf8.out slides_utf8.out.bak slides_utf8.snm slides_utf8.toc
