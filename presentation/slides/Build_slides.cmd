dab1ab10dd8ff4c3bbdcac301ff1f10e30a78636
Fan Yang <stoneyang0915@gmail.com>
2014-10-15 17:03:47 +0800

    ENH: edited gac with presentation

--- presentation/slides/Build_slides.cmd
+++ presentation/slides/Build_slides.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-del /F /Q slides_utf8 slides_utf8.aux slides_utf8.dvi slides_utf8.log slides_utf8.nav slides_utf8.out slides_utf8.out.bak slides_utf8.pdf slides_utf8.snm slides_utf8.toc
+del /F /Q slides_utf8 slides_utf8.aux slides_utf8.dvi slides_utf8.log slides_utf8.nav slides_utf8.out slides_utf8.out.bak slides_utf8.pdf slides_utf8.ps slides_utf8.snm slides_utf8.toc
 latex slides_utf8
 latex slides_utf8
 dvips slides_utf8
