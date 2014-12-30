78d498e3e4d1a1d48d2c897fcf093013d3d3afc3
Fan Yang <stoneyang0915@gmail.com>
2014-12-30 14:19:15 +0800

    ENH: removed the unnecessary files

--- paperwork/Build.cmd
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-call del /Q /S *.aux *.bak *.bbl *.blg *.dvi *.gz *.gz(busy) *.lof *.log *.lot *.toc *.out *.pdf
-call pdflatex -c-style-errors contributions.tex
-call del /Q /S *.aux *.bak *.bbl *.blg *.gz *.lof *.log *.lot *.toc *.out
